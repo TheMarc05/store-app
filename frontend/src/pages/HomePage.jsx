8c7831b77981630214a1ea9e1dcd016977e60234
TheMarc05 <corneamarc@gmail.com>
2025-08-04 21:01:30 +0300

    implement fetch products function

--- frontend/src/pages/HomePage.jsx
+++ frontend/src/pages/HomePage.jsx
@@ -1,7 +1,16 @@
-import { Container, VStack, Text } from "@chakra-ui/react";
+import { useProductStore } from "../store/product";
+import { Container, VStack, Text, SimpleGrid } from "@chakra-ui/react";
+import { useEffect } from "react";
 import { Link } from "react-router-dom";
 
 const HomePage = () => {
+  const { fetchProducts, products } = useProductStore();
+
+  useEffect(() => {
+    fetchProducts();
+  }, [fetchProducts]);
+  console.log("products", products);
+
   return (
     <Container maxW="container.xl" py={12}>
       <VStack spacing={8}>
@@ -14,6 +23,12 @@ const HomePage = () => {
         >
           Current Products
         </Text>
+
+        <SimpleGrid
+          columns={{ base: 1, md: 2, lg: 3 }}
+          spacing={10}
+          w={"full"}
+        ></SimpleGrid>
 
         <Text
           fontSize={"xl"}
